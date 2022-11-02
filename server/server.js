4485963c0c020268d02ea97839f4620f5c27cea8
Benjamin Cook <benjamin.cook@devmounta.in>
2022-11-02 12:14:21 -0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const {home} = require("./controllers/pageCtrl")
+
+app.get("/", home)
+
+
+const { PORT } = process.env
+
+console.log(__dirname)
+app.listen(PORT, () => console.log(`Server lightening on port ${PORT}`))
